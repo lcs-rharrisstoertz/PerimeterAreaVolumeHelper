52677fdf8343908fbf3b07998f3c0ac9d0f5b972
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-01-29 11:24:13 -0500

    copy and pasted the code in

--- PerimeterAreaVolumeHelper/main.swift
+++ PerimeterAreaVolumeHelper/main.swift
@@ -9,3 +9,16 @@ import Foundation
 
 print("Hello, World!")
 
+/// Finds the area of a rectangle
+/// - Parameter length: length of rectangle
+/// - Parameter width: width of rectangle
+/// - Returns: area of the rectangle
+func areaOfRectangle(length: Double, width: Double) -> Double {
+    
+    return length * width
+    
+}
+
+func areaOfCircle(radius: Double) -> Double {
+    return Double.pi * pow(radius, 2.0)
+}
