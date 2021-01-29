9a3baa557ef4477db34bbe8b5e2015a17ba0a800
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-01-29 11:48:21 -0500

    wrote cylinder surface area function

--- PerimeterAreaVolumeHelper/main.swift
+++ PerimeterAreaVolumeHelper/main.swift
@@ -7,7 +7,11 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: Functions
+
+func surfaceAreaOfCylinder(radius: Double, height: Double) -> Double {
+    return 2.0 * Double.pi * pow(radius, 2.0)
+}
 
 /// Finds the area of a rectangle
 /// - Parameter length: length of rectangle
@@ -19,6 +23,22 @@ func areaOfRectangle(length: Double, width: Double) -> Double {
     
 }
 
+/// Finds the area of a circle
+/// - Parameter radius: the straight line distance from the center of a circle to its outer edge
+/// - Returns: area of the circle
 func areaOfCircle(radius: Double) -> Double {
     return Double.pi * pow(radius, 2.0)
 }
+
+// MARK: Input
+let givenLength = 10.0 //set to a double using "type inference"
+let givenWidth: Double = 2.0 //set to double using "type annotation"
+
+
+//MARK: Process
+let area = areaOfRectangle(length: givenLength, width: givenWidth)
+
+
+// MARK: Output
+print ("The area is \(area) square units") //string interpolation
+
