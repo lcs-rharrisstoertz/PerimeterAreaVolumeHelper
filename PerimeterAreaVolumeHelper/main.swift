3a1e336a05ff3ea1f59c58c55101a3ef21ec3b1f
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-01-29 12:19:19 -0500

    removed unnecessary example code

--- PerimeterAreaVolumeHelper/main.swift
+++ PerimeterAreaVolumeHelper/main.swift
@@ -65,32 +65,11 @@ func surfaceAreaOfTriangularPrism (triangleSideA: Double, triangleSideB: Double,
     return triangleSideB*triangleLength + triangleSideA*height + triangleSideB*height + triangleSideC*height
 }
 
-/// Finds the area of a rectangle
-/// - Parameter length: length of rectangle
-/// - Parameter width: width of rectangle
-/// - Returns: area of the rectangle
-func areaOfRectangle(length: Double, width: Double) -> Double {
-    
-    return length * width
-    
-}
-
-/// Finds the area of a circle
-/// - Parameter radius: the straight line distance from the center of a circle to its outer edge
-/// - Returns: area of the circle
-func areaOfCircle(radius: Double) -> Double {
-    return Double.pi * pow(radius, 2.0)
-}
-
 // MARK: Input
-let givenLength = 10.0 //set to a double using "type inference"
-let givenWidth: Double = 2.0 //set to double using "type annotation"
 
 
 //MARK: Process
-let area = areaOfRectangle(length: givenLength, width: givenWidth)
 
 
 // MARK: Output
-print ("The area is \(area) square units") //string interpolation
 
